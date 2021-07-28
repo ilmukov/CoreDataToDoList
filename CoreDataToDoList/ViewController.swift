80e62c9673f7bd0a459d7d10b3a81adab8c729ec
ilmukov <30494462+ilmukov@users.noreply.github.com>
2021-07-28 22:18:31 +0300

    Initial Commit

---
+++ CoreDataToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataToDoList
+//
+//  Created by Stepan Ilmukov on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
